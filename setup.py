e292df5209a7f46921e6d8f84e13d5a654d74a9f
fyrestone <fyrestone@outlook.com>
2017-09-03 17:43:44 +0800

    Add setup.cfg & setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+dic = {}
+exec(open('pydevd_reload/pydevd_reload.py').read(), dic)
+VERSION = dic['__version__']
+
+
+if __name__ == '__main__':
+    setup(name='pydevd_reload',
+          version=VERSION,
+          description='An enhanced reload module from pydev',
+          long_description=open('README.rst').read(),
+          author='fyrestone',
+          author_email='fyrestone@outlook.com',
+          url='https://github.com/fyrestone/pydevd_reload',
+          license="Eclipse Public License",
+          package_dir={'': 'pydevd_reload'},
+          py_modules=['pydevd_reload'],
+          keywords="reload generic utility",
+          platforms=["All"],
+          classifiers=['Development Status :: 5 - Production/Stable',
+                       'Intended Audience :: Developers',
+                       'License :: OSI Approved :: Eclipse Public License',
+                       'Natural Language :: English',
+                       'Operating System :: OS Independent',
+                       'Programming Language :: Python :: 2',
+                       'Programming Language :: Python :: 3',
+                       'Topic :: Software Development :: Libraries',
+                       'Topic :: Utilities'],
+          test_suite='tests',
+          zip_safe=False)
